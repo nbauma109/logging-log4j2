672767ca34ae875794b06b89bb33a8e395249512
nbauma109 <nbauma109@users.noreply.github.com>
2022-08-06 16:38:51 +0200

    Refactoring of labelled breaks
    Is there really a need for labelled breaks here ?

--- log4j-core/src/main/java/org/apache/logging/log4j/core/util/JsonUtils.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/util/JsonUtils.java
@@ -78,17 +78,15 @@ public final class JsonUtils {
         int inPtr = 0;
         final int inputLen = input.length();
 
-        outer:
         while (inPtr < inputLen) {
-            tight_loop:
             while (true) {
                 final char c = input.charAt(inPtr);
                 if (c < escCodeCount && ESC_CODES[c] != 0) {
-                    break tight_loop;
+                    break;
                 }
                 output.append(c);
                 if (++inPtr >= inputLen) {
-                    break outer;
+                    return;
                 }
             }
             // something to escape; 2 or 6-char variant?
